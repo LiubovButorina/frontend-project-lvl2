883216acccd89f8b8e33a953dfdcbbdd99b58110
liubovbutorina <liubovbutorina7@gmail.com>
2021-05-21 16:08:51 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,11 @@
 import program from 'commander';
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .parse();
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .parse();
 
 const options = program.opts();
 if (options.help === true) {
-  console.log(program.help());
+  program.help();
 }
