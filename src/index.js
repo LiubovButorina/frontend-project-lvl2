03bc90b355f2febcf08ea38da136ca3d7ae9b6d1
liubovbutorina <liubovbutorina7@gmail.com>
2021-08-30 18:43:38 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,30 +2,40 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
-const genDiff = (pathToFile1, pathToFile2) => {
-  const path1 = path.resolve(pathToFile1);
-  const path2 = path.resolve(pathToFile2);
-  const data1 = fs.readFileSync(path1, 'utf-8');
+const getFileData = (filepath) => {
+  const data = fs.readFileSync(path.resolve(filepath), 'utf-8');
+  return data;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = getFileData(filepath1);
+  const data2 = getFileData(filepath2);
   const object1 = JSON.parse(data1);
-  const data2 = fs.readFileSync(path2, 'utf-8');
   const object2 = JSON.parse(data2);
   const commonObjects = { ...object1, ...object2 };
   const keys = Object.keys(commonObjects).sort();
+  const newObj = {};
   const result = keys.flatMap((key) => {
     if (_.has(object1, key)) {
       if (_.has(object2, key)) {
         if (object1[key] === object2[key]) {
+          newObj[`  ${key}`] = object1[key];
           return `  ${key}: ${object1[key]}`;
         }
+        newObj[`- ${key}`] = object1[key];
+        newObj[`+ ${key}`] = object2[key];
         return [`- ${key}: ${object1[key]}`, `+ ${key}: ${object2[key]}`];
       }
+      newObj[`- ${key}`] = object1[key];
       return `- ${key}: ${object1[key]}`;
     }
+    newObj[`+ ${key}`] = object2[key];
     return `+ ${key}: ${object2[key]}`;
   });
-  const str = result.join('\n  ');
-  const strFormatted = `{\n  ${str}\n}`;
-  console.log(strFormatted);
+  let formattedResult = result.join('\n  ');
+  formattedResult = `{\n  ${formattedResult}\n}`;
+  const resultJSON = JSON.stringify(newObj);
+  return [resultJSON, formattedResult];
 };
 
 export default genDiff;
