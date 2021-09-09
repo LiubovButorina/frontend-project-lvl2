cbb51cc890ab0a0eee0f1cc1b6e5ffe03627aca5
liubovbutorina <liubovbutorina7@gmail.com>
2021-09-09 17:33:56 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -2,53 +2,45 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
-const getFileData = (filepath) => {
+const makeFileData = (filepath) => {
   const data = fs.readFileSync(path.resolve(filepath), 'utf-8');
   return data;
 };
 
-const getFilesData = (filepath1, filepath2) => {
-  const data1 = getFileData(filepath1);
-  const data2 = getFileData(filepath2);
-  // const object1 = JSON.parse(data1);
-  // const object2 = JSON.parse(data2);
-  // return [object1, object2];
-  return [data1, data2];
-};
-
-const getObjectsData = (data1, data2) => {
-  const object1 = JSON.parse(data1);
-  const object2 = JSON.parse(data2);
-  return [object1, object2];
-};
-
-const genDiff = (filepath1, filepath2) => {
-  const [data1, data2] = getFilesData(filepath1, filepath2);
-  const [object1, object2] = getObjectsData(data1, data2);
-  const commonObjects = { ...object1, ...object2 };
-  const keys = Object.keys(commonObjects).sort();
+const buildDiff = (parseBefore, parseAfter) => {
+  const commonParse = { ...parseBefore, ...parseAfter };
+  const keys = Object.keys(commonParse).sort();
   const newObj = {};
   const result = keys.flatMap((key) => {
-    if (_.has(object1, key)) {
-      if (_.has(object2, key)) {
-        if (object1[key] === object2[key]) {
-          newObj[`  ${key}`] = object1[key];
-          return `  ${key}: ${object1[key]}`;
+    if (_.has(parseBefore, key)) {
+      if (_.has(parseAfter, key)) {
+        if (parseBefore[key] === parseAfter[key]) {
+          newObj[`  ${key}`] = parseBefore[key];
+          return `  ${key}: ${parseBefore[key]}`;
         }
-        newObj[`- ${key}`] = object1[key];
-        newObj[`+ ${key}`] = object2[key];
-        return [`- ${key}: ${object1[key]}`, `+ ${key}: ${object2[key]}`];
+        newObj[`- ${key}`] = parseBefore[key];
+        newObj[`+ ${key}`] = parseAfter[key];
+        return [`- ${key}: ${parseBefore[key]}`, `+ ${key}: ${parseAfter[key]}`];
       }
-      newObj[`- ${key}`] = object1[key];
-      return `- ${key}: ${object1[key]}`;
+      newObj[`- ${key}`] = parseBefore[key];
+      return `- ${key}: ${parseBefore[key]}`;
     }
-    newObj[`+ ${key}`] = object2[key];
-    return `+ ${key}: ${object2[key]}`;
+    newObj[`+ ${key}`] = parseAfter[key];
+    return `+ ${key}: ${parseAfter[key]}`;
   });
   let formattedResult = result.join('\n  ');
   formattedResult = `{\n  ${formattedResult}\n}`;
   const resultJSON = JSON.stringify(newObj);
   return [resultJSON, formattedResult];
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const beforeConfig = makeFileData(filepath1);
+  const afterConfig = makeFileData(filepath2);
+
+  const parseBefore = JSON.parse(beforeConfig);
+  const parseAfter = JSON.parse(afterConfig);
+  return buildDiff(parseBefore, parseAfter);
 };
 
 export default genDiff;
